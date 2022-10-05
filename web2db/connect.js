1e0de7721b20455176f52d2e1b62aac5dc774eac
allen20021005 <113204519+allen20021005@users.noreply.github.com>
2022-10-05 15:21:00 +0800

    web2

---
+++ web2db/connect.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+    host:"localhost" ,
+    user:"root",
+    password:"Allen911005"
+});
+
+con.connect(function(err){
+    if(err)throw err;
+    console.log("connect!");
+});
